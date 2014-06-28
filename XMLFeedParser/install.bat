7c69c0667f24f2f475129e77a0132654010bde79
pgescudero <pgescudero@gmail.com>
2014-06-28 15:11:21 +1000

    install.bat

--- XMLFeedParser/install.bat
+++ XMLFeedParser/install.bat
@@ -1,8 +1,8 @@
 net stop XMLFeedParser
 
-C:\Windows\Microsoft.NET\Framework\v4.0.30319\InstallUtil.exe -u /username=WIN-2993Q9KORLC\Administrator /password=K3c.Ym?78= /unattended C:\Dropbox\XMLFeedParser\XMLFeedParser.exe
+C:\Windows\Microsoft.NET\Framework\v4.0.30319\InstallUtil.exe -u /username=WIN-2993Q9KORLC\Administrator /password=K3c.Ym?78= /unattended C:\repos\XMLParser_src\XMLFeedParser\bin\Release\XMLFeedParser.exe
 
-C:\Windows\Microsoft.NET\Framework\v4.0.30319\InstallUtil.exe /username=WIN-2993Q9KORLC\Administrator /password=K3c.Ym?78= /unattended C:\Dropbox\XMLFeedParser\XMLFeedParser.exe
+C:\Windows\Microsoft.NET\Framework\v4.0.30319\InstallUtil.exe /username=WIN-2993Q9KORLC\Administrator /password=K3c.Ym?78= /unattended C:\repos\XMLParser_src\XMLFeedParser\bin\Release\XMLFeedParser.exe
 
 net start XMLFeedParser
 
