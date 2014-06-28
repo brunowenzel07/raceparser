226301c14e6c331f4f13c34a721b259836d53fad
pgescudero <pgescudero@gmail.com>
2014-06-28 15:01:05 +1000

    install.bat

--- XMLFeedParser/build.bat
+++ XMLFeedParser/build.bat
@@ -1,2 +1,2 @@
-"C:\Program Files (x86)\MSBuild\12.0\Bin\msbuild.exe" ..\XMLFeedParser.sln /t:Build
+"C:\Program Files (x86)\MSBuild\12.0\Bin\msbuild.exe" ..\XMLFeedParser.sln /t:Build /p:Configuration=Release
 pause
---
+++ XMLFeedParser/install.bat
@@ -0,0 +1,9 @@
+net stop XMLFeedParser
+
+C:\Windows\Microsoft.NET\Framework\v4.0.30319\InstallUtil.exe -u /username=WIN-2993Q9KORLC\Administrator /password=K3c.Ym?78= /unattended C:\Dropbox\XMLFeedParser\XMLFeedParser.exe
+
+C:\Windows\Microsoft.NET\Framework\v4.0.30319\InstallUtil.exe /username=WIN-2993Q9KORLC\Administrator /password=K3c.Ym?78= /unattended C:\Dropbox\XMLFeedParser\XMLFeedParser.exe
+
+net start XMLFeedParser
+
+pause
